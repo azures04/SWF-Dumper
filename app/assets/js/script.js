3daddb227a7a56083b4a735550a190d522c10f1a
Azures <gilleslazure04@gmail.com>
2023-11-12 23:04:11 +0100

    Adjusting the titlebar

---
+++ app/assets/js/script.js
@@ -0,0 +1,19 @@
+function handleResize(boolean) {
+    if (boolean) {
+        document.querySelector("main").style.top = "0px"
+        document.querySelector("main").style.left = "0px"
+        document.querySelector("main").style.right = "0px"
+        document.querySelector("main").style.bottom = "0px"
+        document.querySelector("main").style.boxShadow = "none"
+        document.querySelector("main").style.borderRadius = "0px"
+        document.querySelector("#titlebar").children[0].style.borderRadius = "0px"
+    } else {
+        document.querySelector("main").style.top = "6px"
+        document.querySelector("main").style.left = "6px"
+        document.querySelector("main").style.right = "6px"
+        document.querySelector("main").style.bottom = "6px"
+        document.querySelector("main").style.boxShadow = "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);"
+        document.querySelector("main").style.borderRadius = "15px"
+        document.querySelector("#titlebar").children[0].style.borderRadius = "15px"
+    }
+}
--- preload.js
+++ preload.js
@@ -1,11 +1,14 @@
 const { contextBridge, ipcRenderer, shell } = require("electron")
 
 contextBridge.exposeInMainWorld("swf_dumper", {
-    application: {
+    window: {
         close() { ipcRenderer.send("action", { action: "close" }) },
         minimize() { ipcRenderer.send("action", { action: "minimize" }) },
         maximize() { ipcRenderer.send("action", { action: "maximize" }) },
-        openDevTools() { ipcRenderer.send("action", { action: "openDevTools" }) },
-        openExternal(url) { shell.openExternal(url) }
+        openExternal(url) { shell.openExternal(url) },
+        handleResize() { ipcRenderer.send("action", { action: "handleResize" }) }
+    },
+    dumper: {
+        dump(input, output) { ipcRenderer.send("action", { action: "dump", data: { input, output } }) }
     }
 })
